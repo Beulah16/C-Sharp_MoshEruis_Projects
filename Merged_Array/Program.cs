77763f619058d95f023a194830783e0de426ff80
Beulah16 <igboanugobeulah16@gmail.com>
2023-03-21 16:15:57 +0100

    Code Projects

---
+++ Merged_Array/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;  
+public class MergedArrays
+{  
+    public static void Main() 
+{   //Questin: Write a program in C# Sharp to merge two arrays of same size sorted in ascending and descending order.
+
+	int[] firstarr;
+	int[] secarr;
+	int[] mergedarr;
+	
+       Console.WriteLine("Hello User! \nThis is a program that merges two arrays sorted in ascendiing and descending order.");
+    /*----------------- Getting array length and elements for the first array------------------------------------*/
+	   Console.Write("Number of elements to be stored in the first array = ");
+	   int firstsize = Convert.ToInt32(Console.ReadLine()); 
+       firstarr = new int[firstsize];	   
+
+       Console.WriteLine($"Input {firstsize} elements in the array :");
+       for(int i=0; i<firstsize; i++)
+        {
+	      Console.Write($"Element [{i}] = ");
+	     firstarr[i] = Convert.ToInt32(Console.ReadLine()); 		  
+	    }
+        
+    /*----------------- Getting array length and elements for the second array------------------------------------*/
+       Console.Write("\nNumber of elements to be stored in the second array = ");
+	   int secsize = Convert.ToInt32(Console.ReadLine()); 
+       secarr = new int[secsize];	   
+   
+       Console.WriteLine($"Input {secsize} elements in the array :");
+       for(int i=0; i<secsize; i++)
+        {
+	      Console.Write($"Element [{i}] = ");
+	      secarr[i] = Convert.ToInt32(Console.ReadLine()); 		  
+	    }
+    /* size of merged array is size of first array +  size of second array */
+        int mergedsize = firstsize + secsize;
+        mergedarr = new int[mergedsize];
+     
+/*----------------- Elements are inserted in the third array------------------------------------*/
+    for(int i=0; i < firstsize; i++)
+        {
+            mergedarr[i] = firstarr[i];
+        }
+        int j = firstsize;
+        int l = 0;
+        while(j < mergedsize)
+        {
+                mergedarr[j] = secarr[l];
+                j +=1;
+                l +=1;            
+        }
+/*----------------- sort the array in ascending order ---------------------------*/
+    Array.Sort(mergedarr);
+    Console.Write("\nArray sorted in ascending order: ");
+    for(int k=0; k < mergedsize; k++)
+    {
+        Console.Write($"{mergedarr[k]} ");
+    }
+/*----------------- sort the array in descending order ---------------------------*/
+    Array.Sort(mergedarr, delegate(int a, int z){return z - a;});
+    Console.Write("\nArray sorted in descending order: ");
+    for(int k=0; k < mergedsize; k++)
+    {
+        Console.Write($"{mergedarr[k]} ");
+    }
+  }
+}
+
---
+++ Occuring_Odd/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class OccuringOdd
+{
+	public static void Main()
+	{
+		int[] arr = {2, 3, 5, 4, 5, 2, 4, 3, 5, 2, 4, 4, 2,};
+		int count2 = 0;
+		int count3 = 0;
+		int count4 = 0;
+		int count5 = 0;
+		for(int i = 0; i < arr.Length; i++)
+		{
+			int temp = arr[i];
+			if(temp == 2)
+			{
+				count2 += 1;
+			}
+			if(temp == 3)
+			{
+				count3 += 1;
+			}
+			if(temp == 4)
+			{
+				count4 += 1;
+			}
+			if(temp == 5)
+			{
+				count5 += 1;
+			}
+		}
+		if(count2 % 2 != 0)
+		{
+			Console.WriteLine("2 is occurring odd number of times");
+		}
+		if(count3 % 2 != 0)
+		{
+			Console.WriteLine("3 is occurring odd number of times");
+		}
+		if(count4 % 2 != 0)
+		{
+			Console.WriteLine("4 is occurring odd number of times");
+		}
+
+		if(count5 % 2 != 0)
+		{
+			Console.WriteLine("5 is occurring odd number of times");
+		}
+	}
+}

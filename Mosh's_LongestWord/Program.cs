706f6b0392d1df782202e8d75740d28089ff4e9a
Beulah16 <igboanugobeulah16@gmail.com>
2023-06-29 17:23:27 +0100

    Introduction to the System.IO namespace

---
+++ Mosh's_LongestWord/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace Mosh
+{
+    class LongestWord
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reads a text file and displays the longest word in the file.
+            var textFile = @"C:/Users/user/Documents/Web3.txt";
+            var words = File.ReadAllText(textFile).Split(" ");
+            var list = new List<string>();
+            var index = new List<int>();
+            foreach (var word in words)
+            {
+                var newWord = word.Replace('\t', ' ').Trim();
+                var length = newWord.Length;
+                list.Add(newWord);
+                index.Add(length);
+            }
+            var longest = index.IndexOf(index.Max());
+            Console.WriteLine("The longest word: \"{0}\"", list[longest]);
+        }
+    }
+}
---
+++ Mosh's_ReadTextFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+namespace Mosh
+{
+    class ReadTextFile
+    {
+        static void Main(string[]args)
+        {
+            //Write a program that reads a text file and displays the number of words.
+            var textFile = @"C:/Users/user/Documents/Web3.txt";
+            var words = File.ReadAllText(textFile).Split(" ");
+            var list = new List<string>();
+            foreach(var word in words)
+            {
+                var newWord = word.Trim();
+                list.Add(newWord);
+            }
+            Console.WriteLine("This file contains {0} words", list.Count);
+        }
+    }
+}
